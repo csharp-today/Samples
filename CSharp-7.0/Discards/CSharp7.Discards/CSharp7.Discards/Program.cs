6cbd76f0e5602761bec74b52e31fd0919e62d9e7
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-08-27 22:45:20 +0200

    C# 7.0 - Discards

---
+++ CSharp-7.0/Discards/CSharp7.Discards/CSharp7.Discards/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace CSharp7.Discards
+{
+    public class Program
+    {
+        public static void Main(string[] args) => new Program().Run();
+
+        public void Run()
+        {
+            Example1();
+            Example2();
+            Example3();
+            Example4();
+            Example5();
+            Example6();
+            Example7();
+            Example8();
+            Example9();
+        }
+
+        // Let's have a method that returns a value
+        private int DoSomethingAndReturnValue() => 24;
+
+        private void Example1()
+        {
+            // Get the value
+            var myValue = DoSomethingAndReturnValue();
+
+            // Ignore the value
+            DoSomethingAndReturnValue();
+        }
+
+        private void DoSomethingAndGiveValue(out int value) => value = 42;
+
+        private void Example2()
+        {
+            int ignoredVariable;
+            DoSomethingAndGiveValue(out ignoredVariable);
+        }
+
+        private void Example3()
+        {
+            DoSomethingAndGiveValue(out var ignoredVariable);
+        }
+
+        private void Example4()
+        {
+            DoSomethingAndGiveValue(out int _);
+        }
+
+        private void Example5()
+        {
+            DoSomethingAndGiveValue(out _);
+
+            // The following won't compile as discards is not a variable that one can read from.
+            // System.Console.WriteLine("Print discard: " + _);
+        }
+
+        private void GetManyValues(out int first, out int second, out int third, out int forth) =>
+            first = second = third = forth = 13;
+
+        private void Example6()
+        {
+            GetManyValues(out _, out int thisOneWeNeed, out _, out _);
+        }
+
+        private void Example7()
+        {
+            // Here is the simplest discard example
+            _ = 1 + 2;
+
+            // The following still won't work
+            // System.Console.WriteLine("Try print discard again: " + _);
+        }
+
+        private void Example8()
+        {
+            var tuple = (1, 1, 2, 3, 5, 8);
+            (_, _, _, _, var fifth, _) = tuple;
+        }
+
+        private void Example9()
+        {
+            object x = 13;
+            if (x is var _)
+            {
+                // Still can't use discard as variable :)
+                // System.Console.WriteLine("Print: " + _);
+            }
+        }
+    }
+}
---
+++ CSharp-7.0/Discards/CSharp7.Discards/CSharp7.Discards/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharp7.Discards")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://csharp.today")]
+[assembly: AssemblyProduct("CSharp7.Discards")]
+[assembly: AssemblyCopyright("Copyright © Mariusz Bojkowski 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b91c5492-0d7a-4f41-98f4-464815ea64df")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
